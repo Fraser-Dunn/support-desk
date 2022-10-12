aece1f50caacc7ea2cce33efc1d0fd8e58af68ec
Fraser Dunn <fraseradunn@gmail.com>
2022-10-12 17:04:34 +0100

    auth service and slice updates

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { toast } from "react-toastify";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { FaUser } from "react-icons/fa";
 import { useSelector, useDispatch } from "react-redux";
-import { register } from "../features/auth/authSlice";
+import { register, reset } from "../features/auth/authSlice";
 
 const Register = () => {
    const [formData, setFormData] = useState({
@@ -16,10 +17,27 @@ const Register = () => {
    const { name, email, password, password2 } = formData;
 
    const dispatch = useDispatch();
+   const navigate = useNavigate();
 
-   const { user, isLoading, isSuccess, message } = useSelector(
-      (state) => state.auth
-   );
+   const {
+      user, //isLoading,
+      isError,
+      isSuccess,
+      message,
+   } = useSelector((state) => state.auth);
+
+   useEffect(() => {
+      if (isError) {
+         toast.error(message);
+      }
+
+      // Redirect when logged in
+      if (isSuccess || user) {
+         navigate("/");
+      }
+
+      dispatch(reset());
+   }, [isError, isSuccess, user, message, navigate, dispatch]);
 
    const onChange = (e) => {
       setFormData((prevState) => ({
