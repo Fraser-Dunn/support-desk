a78f112984f19fc6f91d467e2ecb3a0f1f289de7
Fraser Dunn <fraseradunn@gmail.com>
2022-10-12 15:42:43 +0100

    Home changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaQuestionCircle, FaTicketAlt } from "react-icons/fa";
 
 const Home = () => {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  )
-}
+   return (
+      <>
+         <section className="heading">
+            <h1>What do you need help with?</h1>
+            <p>Please choose from an option below</p>
+         </section>
 
-export default Home
+         <Link
+            to="/new-ticket"
+            className="btn btn-reverse btn-block"
+         >
+            <FaQuestionCircle /> Create New Ticket
+         </Link>
+         <Link
+            to="/tickets"
+            className="btn btn-block"
+         >
+            <FaTicketAlt /> View My Tickets
+         </Link>
+      </>
+   );
+};
+
+export default Home;
