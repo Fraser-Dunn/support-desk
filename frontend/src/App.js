f0ef42ca8ea03ef352bdb7f3b8d1c75007987855
Fraser Dunn <fraseradunn@gmail.com>
2022-10-12 15:34:40 +0100

    register page complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -11,12 +13,22 @@ function App() {
             <div className="container">
                <Header />
                <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/login" element={<Login />} />
-                  <Route path="/register" element={<Register />} />
+                  <Route
+                     path="/"
+                     element={<Home />}
+                  />
+                  <Route
+                     path="/login"
+                     element={<Login />}
+                  />
+                  <Route
+                     path="/register"
+                     element={<Register />}
+                  />
                </Routes>
             </div>
          </Router>
+         <ToastContainer />
       </>
    );
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {FaSignInAlt, FaSignOutAlt, FaUser} from "react-icons/fa"
+import {FaSignInAlt, FaUser} from "react-icons/fa"
 import {Link} from "react-router-dom"
 
 const Header = () => {
