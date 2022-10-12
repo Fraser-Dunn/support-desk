28cff52375966c51742a90f9a754f781b2ff3cd2
Fraser Dunn <fraseradunn@gmail.com>
2022-10-12 15:38:36 +0100

    Log in page set up

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,69 @@
-import React from 'react'
+import React from "react";
+//import { toast } from "react-toastify";
+import { useState } from "react";
+import { FaSignInAlt } from "react-icons/fa";
 
 const Login = () => {
-  return (
-    <div><h1>
-      Login</h1></div>
-  )
-}
+   const [formData, setFormData] = useState({
+      email: "",
+      password: "",
+   });
 
-export default Login
+   const { email, password } = formData;
+
+   const onChange = (e) => {
+      setFormData((prevState) => ({
+         ...prevState,
+         [e.target.name]: e.target.value,
+      }));
+   };
+
+   const onSubmit = (e) => {
+      e.preventDefault();
+   };
+
+   return (
+      <>
+         <section className="heading">
+            <h1>
+               <FaSignInAlt /> Log in
+            </h1>
+            <p>Please log in to get support</p>
+         </section>
+
+         <section className="form">
+            <form onSubmit={onSubmit}>
+               <div className="form-group">
+                  <input
+                     type="email"
+                     className="form-control"
+                     id="email"
+                     name="email"
+                     value={email}
+                     onChange={onChange}
+                     placeholder="Enter your email"
+                     required
+                  />
+               </div>
+               <div className="form-group">
+                  <input
+                     type="password"
+                     className="form-control"
+                     id="password"
+                     name="password"
+                     value={password}
+                     onChange={onChange}
+                     placeholder="Enter your password"
+                     required
+                  />
+               </div>
+               <div className="form-group">
+                  <button className="btn btn-block">Submit</button>
+               </div>
+            </form>
+         </section>
+      </>
+   );
+};
+
+export default Login;
