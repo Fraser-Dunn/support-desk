a77831c857995027093c4c89bbe804cd5bc894e1
Fraser Dunn <fraseradunn@gmail.com>
2022-10-18 17:46:31 +0100

    new ticket page fleshed out

--- frontend/src/pages/NewTicket.jsx
+++ frontend/src/pages/NewTicket.jsx
@@ -1,7 +1,76 @@
-import React from "react";
+import { useState } from "react";
+import { useSelector } from "react-redux";
 
-const newTicket = () => {
-   return <h1>New Ticket</h1>;
+const NewTicket = () => {
+   const { user } = useSelector((state) => state.auth);
+   const [name, setName] = useState(user.name);
+   const [email] = useState(user.email);
+   const [product, setProduct] = useState("iPhone");
+   const [description, setDescription] = useState("");
+
+   const onSubmit = (e) => {
+      e.preventDefault();
+   };
+
+   return (
+      <>
+         <section className="heading">
+            <h1>New Ticket</h1>
+            <p>Please fill out the form below</p>
+         </section>
+
+         <section className="form">
+            <div className="form-group">
+               <label htmlFor="name">Customer Name</label>
+               <input
+                  type="text"
+                  className="form-control"
+                  value={name}
+                  disabled
+               />
+            </div>
+            <div className="form-group">
+               <label htmlFor="name">Customer Email</label>
+               <input
+                  type="text"
+                  className="form-control"
+                  value={email}
+                  disabled
+               />
+            </div>
+            <form onSubmit={onSubmit}>
+               <div className="form-group">
+                  <label htmlFor="product">Product</label>
+                  <select
+                     name="product"
+                     id="product"
+                     value={product}
+                     onChange={(e) => setProduct(e.target.value)}
+                  >
+                     <option value="iPhone">iPhone</option>
+                     <option value="iPhone">Macbook Pro</option>
+                     <option value="iPhone">iMac</option>
+                     <option value="iPhone">iPad</option>
+                  </select>
+               </div>
+               <div className="form-group">
+                  <label htmlFor="description">Description of the issue</label>
+                  <textarea
+                     name="description"
+                     id="description"
+                     className="form-control"
+                     placeholder="Description"
+                     value={description}
+                     onChange={(e) => setDescription(e.target.value)}
+                  ></textarea>
+               </div>
+               <div className="form-group">
+                  <button className="btn btn-block">Submit</button>
+               </div>
+            </form>
+         </section>
+      </>
+   );
 };
 
-export default newTicket;
+export default NewTicket;
