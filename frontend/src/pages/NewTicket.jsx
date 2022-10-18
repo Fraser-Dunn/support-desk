31d1a6d0138c21bf2691abb454ba9b41415edf9a
Fraser Dunn <fraseradunn@gmail.com>
2022-10-18 18:21:51 +0100

    creating a new ticket working and added a back button

--- frontend/src/pages/NewTicket.jsx
+++ frontend/src/pages/NewTicket.jsx
@@ -1,19 +1,49 @@
-import { useState } from "react";
-import { useSelector } from "react-redux";
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { createTicket, reset } from "../features/tickets/ticketSlice";
+import Spinner from "../components/Spinner";
+import BackButton from "../components/BackButton";
 
 const NewTicket = () => {
    const { user } = useSelector((state) => state.auth);
+   const { isLoading, isError, isSuccess, message } = useSelector(
+      (state) => state.ticket
+   );
+
    const [name] = useState(user.name);
    const [email] = useState(user.email);
    const [product, setProduct] = useState("iPhone");
    const [description, setDescription] = useState("");
 
+   const dispatch = useDispatch();
+   const navigate = useNavigate();
+
+   useEffect(() => {
+      if (isError) {
+         toast.error(message);
+      }
+
+      if (isSuccess) {
+         dispatch(reset());
+         navigate("/tickets");
+      }
+      dispatch(reset());
+   }, [dispatch, isError, isSuccess, navigate, message]);
+
    const onSubmit = (e) => {
       e.preventDefault();
+      dispatch(createTicket({ product, description }));
    };
+
+   if (isLoading) {
+      return <Spinner />;
+   }
 
    return (
       <>
+         <BackButton url="/" />
          <section className="heading">
             <h1>New Ticket</h1>
             <p>Please fill out the form below</p>
@@ -48,9 +78,9 @@ const NewTicket = () => {
                      onChange={(e) => setProduct(e.target.value)}
                   >
                      <option value="iPhone">iPhone</option>
-                     <option value="iPhone">Macbook Pro</option>
-                     <option value="iPhone">iMac</option>
-                     <option value="iPhone">iPad</option>
+                     <option value="Macbook Pro">Macbook Pro</option>
+                     <option value="iMac">iMac</option>
+                     <option value="iPad">iPad</option>
                   </select>
                </div>
                <div className="form-group">
